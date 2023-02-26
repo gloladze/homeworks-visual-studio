e376d56a40490d4dc392639ef2e1e7c991051607
gloladze <abudia@bk.ru>
2023-02-26 22:08:31 +0400

    add new solution

---
+++ homework/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Giorgi Loladze");
+            
+            
+        }
+    }
+}
